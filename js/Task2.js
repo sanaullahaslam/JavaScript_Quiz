5087cff178a62aa6d922ab3aebe318d287930a66
sanaullahaslam <sanaullahaslam09@gmail.com>
2023-09-25 00:38:42 -0700

    Task 2

---
+++ js/Task2.js
@@ -0,0 +1,10 @@
+const array = [10, 20, 30, 40, 50];
+
+const result = array.map((value, index) => `Value at index ${index} is ${value}`);
+
+result.forEach((item) => {
+
+    console.log(item);
+
+                        }
+              );
